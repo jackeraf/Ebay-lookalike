e25f0ff0488902982b7bf4fcfea2fef23e08b2da
jackeraf <jarekimar@yahoo.es>
2016-12-28 13:16:11 +0100

    limiting products and ordering them by prices

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.limit(10).order(:price => 'ASC')
     username
   end
 
