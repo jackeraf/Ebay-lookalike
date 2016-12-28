e9d1fd5a6a8e79018ba43f9082c1152b6c8e3fc4
jackeraf <jarekimar@yahoo.es>
2016-12-28 14:13:47 +0100

    printing random images all

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,14 +2,16 @@ class PageController < ApplicationController
 
 	def index
 
-  	
+  	products= Product.order("RANDOM()").limit(10)
+    @images= products.map do |product| product.image_url end
+      
   	if user_signed_in?
   		@username= current_user.username.capitalize
       
   	else
   		render "index"
   	end
-
+     
   end
 
   def about_the_page
