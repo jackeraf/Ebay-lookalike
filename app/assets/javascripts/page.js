8015db1ee78adaabd45ad52d2e254d935ec8d5a6
jackeraf <jarekimar@yahoo.es>
2016-12-28 13:27:48 +0100

    on the way of printing random images

---
+++ app/assets/javascripts/page.js
@@ -0,0 +1,15 @@
+
+
+// var $numberofSlides = $('.item').length;
+// var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+
+// $('.carousel-indicators li').each(function(){
+//   var $slideValue = $(this).attr('data-slide-to');
+//   if($currentSlide == $slideValue) {
+//     $(this).addClass('active');
+//     $item.eq($slideValue).addClass('active');
+//   } else {
+//     $(this).removeClass('active');
+//     $item.eq($slideValue).removeClass('active');
+//   }
+// });
