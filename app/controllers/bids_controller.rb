4703708b78ffa6f605051635dc2094b07fdb93f9
jackeraf <jarekimar@yahoo.es>
2016-12-31 16:39:43 +0100

    before bids on show 6

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,46 +1,48 @@
 class BidsController < ApplicationController
 	before_action :find_bid, only: [:show, :update, :destroy, :edit]
-  before_action :find_product
-    before_action :authenticate_user!, only: [:new, :edit]
-
-  
-  def create
-    @bid = Bid.new(bid_params)
-    @bid.amount = params[:quantity].to_i
-    @bid.product_id = @product.id
-  	@bid.user_id = current_user.id
+	before_action :find_product
+	before_action :authenticate_user!, only: [:new, :edit]
 
 
-  	@bids = Bid.all.order("created_at DESC") 
+	def create
+		byebug
+		@bid = Bid.new(bid_params)
+		@bid.amount = params[:quantity].to_i
+		@bid.product_id = params[:product_id].to_i
+		@bid.user_id = current_user.id
+
+
+		@bids = Bid.all.order("created_at DESC") 
 		@highest_bid = @bids.limit(1)
 		@bids = @bids.limit(5)
 		
 
-    if @bid.save
-    	redirect_to  new_product_bid_path
-    else
+		if @bid.save
+			redirect_to  product_path(@product)
+			# redirect_to  new_product_bid_path
+		else
 			if @bid.errors
-			flash[:error] = @bid.errors.full_messages[0]
+				flash[:error] = @bid.errors.full_messages[0]
 			end
-    	redirect_to  new_product_bid_path
+			redirect_to  product_path(@product)
 		end
 
-  end
+	end
 
-  private
+	private
 
-  def bid_params
-    params.require(:bid).permit(:amount)
-  end
+	def bid_params
+		params.require(:bid).permit(:amount)
+	end
 
-  def find_bid
-  	@bid = Bid.find_by(id: params[:id])
+	def find_bid
+		@bid = Bid.find_by(id: params[:id])
 
-  end
+	end
 
-  def find_product
-  	@product = Product.find_by(id: params[:product_id])
-  end
+	def find_product
+		@product = Product.find_by(id: params[:product_id])
+	end
 
 end
-end
+
