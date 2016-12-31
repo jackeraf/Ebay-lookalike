3c0120ab896e0f1603c49a2bc629ecb923a0145e
jackeraf <jarekimar@yahoo.es>
2016-12-31 14:46:23 +0100

    before creating bids

---
+++ app/controllers/bids_controller.rb
@@ -0,0 +1,43 @@
+class BidsController < ApplicationController
+	before_action :find_bid, only: [:show, :update, :destroy, :edit]
+  before_action :find_product
+    before_action :authenticate_user!, only: [:new, :edit]
+
+  
+  def create
+    @bid = Bid.new(bid_params)
+    @bid.product_id = @product.id
+  	@bid.user_id = current_user.id
+
+  	
+  	@bids = Bid.all.order("created_at DESC") 
+		@bids = @bids.limit(5)
+
+    if @bid.save
+    	redirect_to  new_product_bid_path
+    else
+			if @bid.errors
+			flash[:error] = @bid.errors.full_messages[0]
+			end
+    	redirect_to  new_product_bid_path
+		end
+
+  end
+
+  private
+
+  def bid_params
+    params.require(:bid).permit(:amount)
+  end
+
+  def find_bid
+  	@bid = Bid.find_by(id: params[:id])
+
+  end
+
+  def find_product
+  	@product = Product.find_by(id: params[:product_id])
+  end
+
+end
+end
